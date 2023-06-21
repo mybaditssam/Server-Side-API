3cb74c2e52962ec0e10245d6fec107685b4fb3d2
mybaditssam <shernandez@econnect.eco>
2023-06-21 18:34:10 -0400

    Added remaining functions and set styling to the site, also completed the read me file.

---
+++ Assets/JScript/main.js
@@ -0,0 +1,126 @@
+var apiKey = `497eedf3e9eeee037c6fd2e6a8721151`;
+var city = ""
+var today = new Date();
+var date = today.getFullYear() +  (today.getMonth() + 1) + "-" + today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date + " " + time;
+
+console.log(dateTime);
+
+
+// This searches for the city 
+function citySearch() {
+  var city = document.getElementById("city").value;
+  document.getElementById("city").innerHTML = city;
+ 
+
+  const cityArrayFromLocalStorage = localStorage.getItem("cityArray");
+  const cityArray = JSON.parse(cityArrayFromLocalStorage) || [];
+
+  for (let i = 0; i < Array.length; i++) {
+   
+    var city = document.getElementById("city").value;
+    
+    cityArray.push(document.getElementById("city").value);
+  }
+
+  localStorage.setItem("cityArray", JSON.stringify(cityArray));
+  
+ 
+  var firstCall = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+  fetch(firstCall)
+    .then((response) => response.text())
+    .then((mapData) => setCoordinates(mapData));
+
+  var secondCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  fetch(secondCall)
+    .then((response) => response.json())
+    .then((currentData) => currentWeather(currentData));
+}
+document.getElementById("searchBtn").addEventListener("click", citySearch);
+
+function setCoordinates(mapData) {
+  var coordinates = JSON.parse(mapData);
+  var lat = coordinates[0].lat;
+  var lon = coordinates[0].lon;
+  var thirdCall = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+  
+  fetch(thirdCall)
+    .then((response) => response.json())
+    .then((forecastData) => weatherData(forecastData))
+    .then(console.log(coordinates));
+}
+
+
+function currentWeather(currentData) {
+  console.log(currentData);
+  var weatherNow = document.getElementById("weatherNow");
+  weatherNow.innerHTML = ""
+ 
+  var cityName = currentData.name;
+  var datetime = new Date();
+  var pic = currentData.weather[0].icon;
+  var description = currentData.weather[0].description;
+  var temp = currentData.main.temp;
+  var humidity = currentData.main.humidity;
+  var windSpeed = currentData.wind.speed;
+  var div = document.createElement("div");
+  div.classList.add("container");
+
+  var currentCard = `<div class="container my-5">
+  <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+    <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+      <h4 class= "card-title ">${datetime.toDateString()}</h4>
+      <h1 class="display-4 fw-bold lh-1">${cityName}</h1>
+      <img src= "http://openweathermap.org/img/wn/${pic}@4x.png"
+      class= "card-img-top"
+      alt= "description"/>
+      <p id= "description">${description}</p>
+      <p class="lead">Temperature: ${temp}°F</p>
+      <p class="lead">WindSpeed: ${windSpeed}</p>
+      <p class="lead">Humidity: ${humidity}%</p>
+    </div>
+  </div>`
+
+  div.innerHTML = currentCard;
+
+  weatherNow.appendChild(div);
+
+  console.log(currentData);
+}
+
+// Function to display the forecast throughout the next 5 days
+function weatherData(forecastData) {
+  var forecast = document.getElementById("forecast");
+  forecast.innerHTML = ""
+  for (let i = 0; i < forecastData.list.length; i++) {
+    const listElement = forecastData.list[i];
+    
+    if (listElement.dt_txt.includes("15:00:00")) {
+      console.log(listElement);
+      var datetime = new Date(listElement.dt * 1000);
+      var pic = listElement.weather[0].icon;
+      var description = listElement.weather[0].description;
+      var temp = listElement.main.temp;
+      var humidity = listElement.main.humidity;
+      var windSpeed = listElement.wind.speed;
+
+      var div = document.createElement("div");
+
+      div.classList.add("card");
+
+      var weatherCard = `<h4 class= "card-title ">${datetime.toDateString()}</h4>
+            <img src= "http://openweathermap.org/img/wn/${pic}@4x.png"
+                class= "card-img-top"
+                alt= "description"/>
+                <p id= "description">${description}</p>
+                <p id= "temp">Temperature: ${temp}°F</p>
+                <p id= "windSpeed">WindSpeed: ${windSpeed}mph</p>
+                <p id= "humidity">Humidity: ${humidity}%</p>`;
+
+      div.innerHTML = weatherCard;
+
+      forecast.appendChild(div);
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Server-Side-API
+# Weather Dashboard
+This is a simple HTML and JavaScript project that allows users to retrieve the current weather and a five-day weather forecast for a specific city.
+
+## Features
+The user can enter a city name or select a city from a list.
+The application retrieves the current weather data and displays it, including the city name, date, weather icon, description, temperature, wind speed, and humidity.
+The application also displays a five-day weather forecast, including the date, weather icon, description, temperature, wind speed, and humidity for each day at 3:00 PM.
+
+## Project Structure
+The project consists of:
+
+- index.html
+- style.css
+- main.js
+
+## Getting Started
+To run the project follow these steps:
+
+1. Clone the project repository or download the source code.
+2. Open the index.html file in a web browser.
+
+## Usage
+Enter a city name in the input field or select a city from the list.
+Click the "Submit" button to retrieve the weather data.
+The current weather information, including the city name, date, weather icon, description, temperature, wind speed, and humidity, will be displayed.
+The five-day weather forecast, including the date, weather icon, description, temperature, wind speed, and humidity for each day at 3:00 PM, will be displayed below the current weather.
+
+## Dependencies
+- Bootstrap version 5.2.3: This project uses Bootstrap for styling the web page and components. The Bootstrap CSS file is included via a CDN link.
+- OpenWeatherMap API: This project utilizes the OpenWeatherMap API to retrieve weather data for the specified city. An API key is required and should be set in the apiKey variable in the JavaScript code.
